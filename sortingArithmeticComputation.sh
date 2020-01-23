7645ed68b6a4a6188afe9defef472514885b9523
kn52 <ashish52922@gmail.com>
2020-01-23 11:45:56 +0530

    ADDED:Dictionary

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -13,6 +13,18 @@ echo "Arithmetic Operation3: $(($c * $a / $b ))"
 
 echo "Arithmetic Operation2: $(($a % $b + $c))"
 
+res1=$(($a + $b * $c))
+res2=$(($a * $b + $c))
+res3=$(($c * $a / $b ))
+res4=$(($a % $b + $c))
+
+arithmeticDict[res1]=$res1
+arithmeticDict[res2]=$res2
+arithmeticDict[res3]=$res3
+arithmeticDict[res4]=$res4
+
+echo "Arithmetic Dictionary: ${arithmeticDict[@]}"
+
 
 
 
